42c5d22c1a01f2159166828e7409d1af20d1debe
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-07-04 12:43:53 +0900

    D1

--- src/D1/ReversePrint.java
+++ src/D1/ReversePrint.java
@@ -1,7 +1,16 @@
 package D1;
 
-public class ReversePrint {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class ReversePrint {
+    public static void main(String[] args) throws Exception{
+
+        Scanner scanner = new Scanner(System.in);
+        int t;
+        t = scanner.nextInt();
+
+        for(int i = t; i>=0; i--){
+            System.out.printf(i + " ");
+        }
     }
 }
