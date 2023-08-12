df6f29d17565d956f4b09de6c8b85c9f0f8b6c7c
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-08-12 18:28:32 +0900

    올바른 괄호 두번째 풂

--- src/highScoreKit/que/올바른괄호.java
+++ src/highScoreKit/que/올바른괄호.java
@@ -2,14 +2,30 @@ package highScoreKit.que;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class 올바른괄호 {
     boolean solution(String s) {
-        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
         boolean answer = true;
 
-        for (int i = 0; i <s.length(); i++){
-            queue.offer(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') { // 열린 괄호면 스택에 추가
+                stack.push(c);
+            } else if (c == ')') { // 닫힌 괄호면 짝을 이루는 열린 괄호를 찾아서 제거
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    answer = false; // 올바르지 않은 괄호
+                    break;
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) { // 스택에 남아있는 열린 괄호가 있다면 올바르지 않은 괄호
+            answer = false;
         }
 
 
