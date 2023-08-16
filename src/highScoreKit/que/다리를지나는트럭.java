10304bee7d985a672c9bc27c422196b8ac3f25e3
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-08-16 17:59:02 +0900

    프로세스 완

---
+++ src/highScoreKit/que/다리를지나는트럭.java
@@ -0,0 +1,38 @@
+package highScoreKit.que;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 다리를지나는트럭 {
+
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 1;
+        int totalWeight = 10;
+
+        Queue<Integer> bridge = new LinkedList<>();
+
+        for(int i =0; i<bridge_length-1;i++){
+            bridge.offer(0);
+        }
+        int currentWeights = truck_weights[0];
+        bridge.offer(currentWeights);
+
+        int index = 1;
+
+        while(!bridge.isEmpty()){
+            answer++;
+            int truck = bridge.poll();
+            currentWeights -= truck;
+
+           if(index < truck_weights.length){
+               if(currentWeights + truck_weights[index] <= weight){
+                   currentWeights += truck_weights[index];
+                   bridge.add(truck_weights[index++]);
+               }else{
+                   bridge.offer(0);
+               }
+           }
+        }
+        return answer;
+    }
+}
--- src/highScoreKit/que/프로세스.java
+++ src/highScoreKit/que/프로세스.java
@@ -8,18 +8,21 @@ public class 프로세스 {
         PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
 
-        for(int i = 0; i<priorities.length; i++){
-           q.offer(priorities[i]);
+        for(int p: priorities){
+           q.offer(p);
         }
-
         while(!q.isEmpty()){
-            for(int i =0; i<priorities.length; i++){
+            for(int i = 0; i<priorities.length; i++) {
                 if(priorities[i] == q.peek()){
                     answer++;
-                    return answer;
+                    if( i == location) {
+                        return answer;
+                    }
                 }
-                q.poll();
-                answer++;
+//                else {  //if(priorities[i] == q.peek()) 부분 이해했다면 필요 없다는 것을 알테야!
+//                    q.poll();
+//                    answer++;
+//                }
             }
         }
         return answer;
