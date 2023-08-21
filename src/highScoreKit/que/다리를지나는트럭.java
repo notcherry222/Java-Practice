4ab6c066f0cc2c130dbe4359bf0b7138aab9f5c6
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-08-21 21:33:20 +0900

    1

--- src/highScoreKit/que/다리를지나는트럭.java
+++ src/highScoreKit/que/다리를지나는트럭.java
@@ -7,7 +7,6 @@ public class 다리를지나는트럭 {
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 1;
-        int totalWeight = 10;
 
         Queue<Integer> bridge = new LinkedList<>();
 
