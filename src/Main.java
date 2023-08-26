c1b9e086a8f5d851a9c3f4167ce38f84014348d7
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-08-26 23:45:06 +0900

    not important

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
+import java.util.ArrayList;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
-    public static void main(String[] args) {
-        String a = "12";
-        String b = "2123";
-        a.substring(0,4).contains(b);
-        
-        System.out.println(a.contains(b));
-        System.out.println(b.contains(a));
+    public String[] solution(String[] names) {
+        ArrayList<String> a = new ArrayList<> ();
+
+        for(int i = 0 ; i<names.length; i++){
+            if(i%5 == 0){
+                a.add(names[i]);
+            }
+        }
+        String[] answer = new String[a.size()];
+        for(int i = 0; i<a.size(); i++){
+            answer[i] = a.get(i);
+        }
+        return answer;
     }
 }
