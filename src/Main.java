8351e31fea41bd3b0116bd03743aff37539d5263
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-08-27 23:06:46 +0900

    rest problem solved

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 import java.util.ArrayList;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.util.Collections;
 
 public class Main {
-    public String[] solution(String[] names) {
-        ArrayList<String> a = new ArrayList<> ();
+        public String[] solution(String myString) {
 
-        for(int i = 0 ; i<names.length; i++){
-            if(i%5 == 0){
-                a.add(names[i]);
+            StringBuilder ans = new StringBuilder();
+            ArrayList<String> answerl = new ArrayList<>();
+
+            for(char str : myString.toCharArray()){
+                if(str !='x'){
+                    ans.append(str);
+                }else
+                {
+                    if(ans.length()>0){
+                        answerl.add(ans.toString());
+                        ans.setLength(0);}}
             }
-        }
-        String[] answer = new String[a.size()];
-        for(int i = 0; i<a.size(); i++){
-            answer[i] = a.get(i);
-        }
-        return answer;
+
+            if(ans.length()>0){ answerl.add(ans.toString());}
+
+            Collections.sort(answerl);
+            String[] answer = new String[answerl.size()];
+            for(int i =0 ; i< answerl.size();i++){
+                answer[i] = answerl.get(i);
+            }
+
+            return answer;
+
     }
 }
