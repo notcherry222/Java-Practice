55097821cbac35084e129beddc7a9faf187b68c5
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-10-01 14:08:48 +0900

    카펫문제 완

--- src/highScoreKit/완전탐색/카펫.java
+++ src/highScoreKit/완전탐색/카펫.java
@@ -1,29 +1,28 @@
 package highScoreKit.완전탐색;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 
 public class 카펫 {
     public int[] solution(int brown, int yellow) {
-        //갈색 + 노란색
-        int total = brown+yellow;
-        //가로길이:a >=세로 길이:b
-        int b =1;
-        int a =1;
-        int j = 0;
-
-        int[] arr = new int[total];
-
-        for(int i = 0; i<=total/2+1 ; i++) {
-            if(total%b==0){
-                a = total/b;
-                arr[j] = a;
-                j++;
-                arr[j] = b;
-                j++;
-            }
-        }
-
-        return arr;
+        int sum = brown + yellow;
+        int inTile = yellow;
+        return find(inTile, sum);
     }
+    private static int[] find(int inTile, int sum){
+            //y=세로, x=가로
+            int y = 0, x=0;
+
+            for(int i =1; i<=inTile ; i++){
+                if(inTile%i == 0){
+                    y = Math.min(i, inTile/i);
+                    x = Math.max(i, inTile/i);
+                    if( ((y+2)*(x+2))==sum){
+                        break;
+                    }
+                }
+            } return new int[] {x+2, y+2};
+    }
+
 }
