bf377d4babc7664e9da31fae016a355a23e7354a
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-12-08 14:59:45 +0900

    pow 사용과 직접 구현한 제곱 구하는 로직 사용의 차이

---
+++ src/백준/브론즈2/분산처리.java
@@ -0,0 +1,35 @@
+package 백준.브론즈2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//1009
+public class 분산처리 {
+    // 컴퓨터 번호 % 10 번호를 가짐
+    // 총 데이터의 개수는 a의 b제곱
+    // 마지막 데이터가 처리될 컴퓨터의 번호
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<t; i++) {
+            String lines[] = br.readLine().split(" ");
+            int a = Integer.parseInt(lines[0]); //a
+            int b = Integer.parseInt(lines[1]); //b
+            int data = 1;
+
+            for(int j = 0; j<b; j++) {
+                data = (data * a) % 10;
+            }
+            if(data == 0 ) {
+                data =  10;
+            }
+
+            System.out.println(data);
+        }
+
+    }
+
+}
+//7931
