4aa087f2c6a953096de42f3bd7eed846f96ec5da
Kim Chae Lim <121853238+notcherry222@users.noreply.github.com>
2023-12-12 12:38:40 +0900

    전체 일수를 구해서 겹치는 만큼 빼면 d-day

--- src/백준/실버/D_DAY.java
+++ src/백준/실버/D_DAY.java
@@ -6,33 +6,55 @@ import java.io.InputStreamReader;
 
 //1308
 public class D_DAY {
+    private static int check (int year) {
+        if(year % 4 ==0 && year % 100 != 0 || year % 400 == 0) {
+            return 1;
+        } else{
+            return 0;
+        }
+    }
+
+    private static int daysInMonth(int month){
+        int[] days = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return days[month];
+    }
+
+    private static int calculate(int year, int month, int day) {
+        int days = 0;
+        for(int i = 1; i<year; i++) {
+            days += 365 + check(i);
+        }
+        for (int i = 1; i<month; i++) {
+            days+=daysInMonth(i);
+            if (i == 2 ) {
+                days += check(year);
+            }
+        }
+        days += day;
+
+        return days;
+    }
+
     public static void main(String[] args) throws IOException {
-        //4, 400으로 나뉘어지는 수 -> 윤년
-        // y+1000 -> gg 출력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] start = new int[3];
         int[] end = new int[3];
         String[] first = br.readLine().split(" ");
         String[] second = br.readLine().split(" ");
-        int answer = 0;
 
         for(int i = 0; i<3; i++) {
            start[i] = Integer.parseInt(first[i]);
            end[i] = Integer.parseInt(second[i]);
         }
-        //같은 해인지? -> 맞다면? 월/일 계산
-        //           -> 아니라면? 윤월인지? -> 맞다면? 366-start
-        //                               -> 아니라면? 365-start
-        //마지막해는 그냥 +하기
 
-        if(start[0] == end[0]) {
+        int a = calculate(end[0], end[1], end[2]);
+        int b = calculate(start[0], start[1],start[2]);
 
+        if (end[0] - start[0] > 1000 ||
+                ( end[0] - start[0] == 1000 && (start[1] < end[1] || start[1] == end[1] && start[2] <= end[2]))) {
+            System.out.println("gg");
         } else {
-            if(start[0] % 4 ==0 || start[0] % 400 == 0) {
-
-            } else{
-
-            }
+            System.out.println("D-" + (a - b));
         }
     }
 }
