4e96f329645af1e4478b0c324143d881c1a95171
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2024-03-08 13:12:33 +0900

    n과 m

---
+++ src/백준/실버/순열과_조합/순열_15651.java
@@ -0,0 +1,36 @@
+package 백준.실버.순열과_조합;
+
+import java.util.*;
+import java.io.*;
+
+public class 순열_15651 {
+    static int n,m;
+    static StringBuilder sb = new StringBuilder();
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());;
+        arr = new int[m];
+
+        dfs(0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int depth) {
+        if(depth == m){
+            for (int i = 0; i<m; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+        for(int i = 1; i<=n; i++) {
+            arr[depth] = i;
+            dfs(depth+1);
+        }
+    }
+}
